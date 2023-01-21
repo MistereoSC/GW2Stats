868dbb7f2beb5ce453718629f0308d193af836a3
MistereoSC <56682008+MistereoSC@users.noreply.github.com>
2023-01-21 23:21:56 +0100

    Token verification draft
    First implementation of token verification. CSS styling and error handling still missing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="main">
-    <div>
+    <div v-if="authenticated==true">
       <CharacterSelect :authToken="urlParamsToken"> </CharacterSelect>
+    </div>
+    <div v-else>
+      <AuthDialog @validated="(token,permissions) => userValidated(token,permissions)"></AuthDialog>
     </div>
   </div>
 </template>
@@ -14,19 +17,30 @@
 
 <script>
 import CharacterSelect from "@/components/CharacterSelect.vue";
+import AuthDialog from "@/components/AuthDialog.vue";
 
 export default {
-  components: { CharacterSelect },
+  components: { CharacterSelect, AuthDialog },
   data() {
     return {
       urlParamsToken: null,
+      authenticated: false,
+      permissions: {}
     };
   },
   created() {
     //retrieve Auth token from URL (?token='GW2AUTHTOKEN')
-    var urlParams = new URLSearchParams(window.location.search);
-    //console.log(urlParams.has('token'));
-    this.urlParamsToken = urlParams.get("token");
+    // var urlParams = new URLSearchParams(window.location.search);
+    // this.urlParamsToken = urlParams.get("token");
   },
+  methods: {
+    userValidated(t,p) {
+      console.log(t)
+      this.urlParamsToken=t
+      this.permissions=p
+      this.authenticated=true
+      
+    }
+  }
 };
 </script>
---
+++ src/components/AuthDialog.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="main-container">
+    <div class="frame">
+      <div class="sub-container">
+        <input class="input-override input-textarea" v-model="tokenInput" placeholder="OAuth Token"/>
+      </div>
+      <div class="sub-container">
+        <button @click="authenticate">Verify</button>
+        <a href="https://account.arena.net/applications" target="_blank">Get your Token</a>
+      </div>
+      <div v-if="errcode!=200&&errcode!=0">
+        <p>Invalid Token</p>
+      </div>
+      <div v-else>
+        <ul class="permissions-list">
+          <li class="permission-li" :class="[permissions.account?'active':'inactive']">Account</li>
+          <li class="permission-li" :class="[permissions.characters?'active':'inactive']">Characters</li>
+          <li class="permission-li" :class="[permissions.inventories?'active':'inactive']">Inventories</li>
+          <li class="permission-li" :class="[permissions.tradingpost?'active':'inactive']">Tradingpost</li>
+          <li class="permission-li" :class="[permissions.wallet?'active':'inactive']">Wallet</li>
+          <li class="permission-li" :class="[permissions.builds?'active':'inactive']">Builds</li>
+          <li class="permission-li" :class="[permissions.unlocks?'active':'inactive']">Unlocks</li>
+          <li class="permission-li" :class="[permissions.progression?'active':'inactive']">Progression</li>
+          <li class="permission-li" :class="[permissions.pvp?'active':'inactive']">PVP</li>
+          <li class="permission-li" :class="[permissions.guilds?'active':'inactive']">Guilds</li>
+        </ul>
+      </div>
+      <div>
+        <button v-if="errcode==200" @click="confirmAction">Confirm</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main-container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+}
+.frame{
+  width: 700px;
+  height: 400px;
+  background-color: var(--color-background-dark);
+  border: 3px solid var(--c-accent-darkred);
+  border-radius: 12px;
+  display: block;
+  padding: 12px;
+}
+.sub-container{
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+.input-textarea{
+  width: 100%;
+  font-size: 11pt;
+  margin-top: 32px;
+}
+.permissions-list{
+  list-style: none;
+}
+.permission-li{
+  
+}
+
+.permission-li.active::before{
+  content: 'o  ';
+  color:green
+}
+.permission-li.inactive::before{
+  content: 'X  ';
+  color:red
+}
+
+
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  emits: ['validated'],
+  data(){
+    return {
+      tokenInput: "",
+      token: "",
+      permissions: {
+        tradingpost: false,
+        characters: false,
+        pvp: false,
+        progression: false,
+        wallet: false,
+        guilds: false,
+        builds: false,
+        account: false,
+        inventories: false,
+        unlocks: false
+      },
+      errcode: 0
+    }
+  },
+  methods: {
+    async authenticate() {
+      var resp = await axios
+        .get("https://api.guildwars2.com/v2/tokeninfo?access_token="+this.tokenInput)
+        .then(res=>{return res})
+        .catch(err=>{return err.response})
+      if(resp.status == 200){
+        if(resp.data.permissions.includes('tradingpost')){this.permissions.tradingpost=true}
+        if(resp.data.permissions.includes('characters')){this.permissions.characters=true}
+        if(resp.data.permissions.includes('pvp')){this.permissions.pvp=true}
+        if(resp.data.permissions.includes('progression')){this.permissions.progression=true}
+        if(resp.data.permissions.includes('wallet')){this.permissions.wallet=true}
+        if(resp.data.permissions.includes('guilds')){this.permissions.guilds=true}
+        if(resp.data.permissions.includes('builds')){this.permissions.builds=true}
+        if(resp.data.permissions.includes('account')){this.permissions.account=true}
+        if(resp.data.permissions.includes('inventories')){this.permissions.inventories=true}
+        if(resp.data.permissions.includes('unlocks')){this.permissions.unlocks=true}
+        this.token=this.tokenInput
+        this.errcode=200
+        console.log(resp)
+      }
+      else{
+        this.errcode=resp.status
+        console.log(resp)
+      }
+    },
+    confirmAction() {
+      this.$emit('validated', this.token, this.permissions)
+    }
+  }
+}
+</script>
